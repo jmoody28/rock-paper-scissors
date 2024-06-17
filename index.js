20bb028541adff78262db5ee513ced1edfb04bed
Jonathan Moody <jonathanmoody15@gmail.com>
2024-06-17 15:12:44 -0400

    index.html: created html file to give basic UI to rps game
    index.js: made changes in code to append data to htl elements and removed playRound function

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 let humanScore = 0;
 let computerScore = 0;
 let draw = 0;
+let round = 0;
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
+
+const result = document.querySelector("#result");
+const player = document.querySelector("#player");
+const bot = document.querySelector("#bot");
+const tie = document.querySelector("#tie");
+const count = document.querySelector("#count");
 
 function getComputerChoice() {
   let count = Math.floor(Math.random() * 3);
@@ -15,42 +37,80 @@ function getHumanChoice() {
 
 function playRound(humanSelection, computerSelection) {
   if (humanSelection === computerSelection) {
-    console.log("tie");
+    result.textContent = "Tie";
     draw++;
+    tie.textContent = draw;
+    round++;
+    count.textContent = round;
   } else if (humanSelection == "rock") {
     if (computerSelection == "paper") {
-      console.log("Computer wins");
+      result.textContent = "Computer gains a point";
       computerScore++;
+      bot.textContent = computerScore;
+      round++;
+      count.textContent = round;
     } else {
-      console.log("You win");
+      result.textContent = "You gain a point";
       humanScore++;
+      player.textContent = humanScore;
+      round++;
+      count.textContent = round;
     }
   } else if (humanSelection == "paper") {
     if (computerSelection == "scissors") {
-      console.log("Computer wins");
+      result.textContent = "Computer gains a point";
       computerScore++;
+      bot.textContent = computerScore;
+      round++;
+      count.textContent = round;
     } else {
-      console.log("You win");
+      result.textContent = "You gain a point";
       humanScore++;
+      player.textContent = humanScore;
+      round++;
+      count.textContent = round;
     }
   } else if (humanSelection == "scissors") {
     if (computerSelection == "rock") {
-      console.log("Computer wins");
+      result.textContent = "Computer gains a point";
       computerScore++;
+      bot.textContent = computerScore;
+      round++;
+      count.textContent = round;
     } else {
-      console.log("You win");
+      result.textContent = "You gain a point";
       humanScore++;
+      player.textContent = humanScore;
+      round++;
+      count.textContent = round;
     }
   }
-}
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
+  if (humanScore == 5) {
+    result.textContent = "Victory! You won 5 rounds.";
+    humanScore = 0;
+    player.textContent = humanScore;
+    computerScore = 0;
+    bot.textContent = computerScore;
+    draw = 0;
+    tie.textContent = draw;
+  } else if (computerScore == 5) {
+    result.textContent = "Defeat! The computer won 5 rounds";
+    humanScore = 0;
+    player.textContent = humanScore;
+    computerScore = 0;
+    bot.textContent = computerScore;
+    draw = 0;
+    tie.textContent = draw;
   }
-  console.log(humanScore, computerScore);
 }
 
-playGame();
+// function playGame() {
+//   for (let i = 0; i < 5; i++) {
+//     const computerSelection = getComputerChoice();
+//     const humanSelection = getHumanChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+//   console.log(humanScore, computerScore);
+// }
+
+// playGame();
